755fa148f9ca0476e097d28d5b039ae0d72fb9e5
olga <olga@bamboo-card.com>
2024-01-09 22:08:22 +0200

    data processing

---
+++ process_data/Queries.py
@@ -0,0 +1,23 @@
+UPDATE_DATA_BY_YEAR_MONTH = """DELETE FROM {table}
+    WHERE index_0 >= {year} AND index_1 >= {month};"""
+
+UPDATE_DATA_BY_YEAR_QUOTER = """DELETE FROM {table}
+    WHERE index_0 >= {year} AND index_1 >= {quoter};"""
+
+UPDATE_DATA_BY_YEAR_QUOTER_MONTH = """DELETE FROM {table}
+    WHERE index_0 >= {year} AND index_1 >= {quoter}, index_2 >= {month};"""
+
+UPDATE_DATA_BY_YEAR_MONTH_DATE = """DELETE FROM {table}
+    WHERE index_0 >= {quoter} AND index_1 >= {month}, index_2 >= {date};"""
+UPDATE_DATA_BY_YEAR = """DELETE FROM {table}
+    WHERE index_0 >= {year}"""
+
+
+ADD_DATA_TO_DB = """INSERT INTO {table}({label_list}) VALUES ({val_qty});"""
+ADD_DATA_TO_DB_LANG = """INSERT INTO {table}({label_list}) VALUES ({val_qty});"""
+
+SELECT_ALL_DATA = """SELECT * FROM {table}"""
+SELECT_NAMES = """SELECT * FROM {table}"""
+
+CREATE_QUERY = """CREATE TABLE IF NOT EXISTS {table} (
+    {columns_with_types});"""
