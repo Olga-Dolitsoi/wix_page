f9e21f7479242a70eaf11efe835d9747d730ad48
olga <olga@bamboo-card.com>
2024-01-09 22:07:24 +0200

    data processing

---
+++ process_data/data_for_plot.py
@@ -0,0 +1,90 @@
+import pandas as pd
+import psycopg2
+import process_data.Queries as Queries
+import process_data.const as const
+
+
+class ProcessDataForPlot:
+    def __init__(self, table, table_names, labels):
+        self._dataframe = None
+        self._dataframe_names = None
+        self.table = table
+        self.table_names = table_names
+        self.labels = labels
+
+    @property
+    def dataframe(self):
+        return self._dataframe
+
+    @dataframe.setter
+    def dataframe(self, val):
+        self._dataframe = val
+
+    @property
+    def dataframe_names(self):
+        return self._dataframe_names
+
+    @dataframe_names.setter
+    def dataframe_names(self, val):
+        self._dataframe_names = val
+
+    @staticmethod
+    def connect_to_db():
+        host = "tradebot-db.cxsxzbu8uttc.eu-north-1.rds.amazonaws.com"
+        database = "postgres"
+        user = "postgres"
+        password = "gIoJwrSmBzx5qME4rh0I"
+        conn_tradebot = psycopg2.connect(
+            host=host,
+            database=database,
+            user=user,
+            password=password)
+        cursor_tradebot = conn_tradebot.cursor()
+        return conn_tradebot, cursor_tradebot
+
+    def get_data_from_db_for_plot(self):
+        conn, cursor = self.connect_to_db()
+        cursor.execute(Queries.SELECT_ALL_DATA.format(table=self.table))
+        all_data = cursor.fetchall()
+        all_data = pd.DataFrame(all_data)
+        all_data.columns = [description[0] for description in cursor.description]
+        return all_data
+
+    def get_names_from_db(self):
+        conn, cursor = self.connect_to_db()
+        cursor.execute(Queries.SELECT_NAMES.format(table=self.table_names))
+        name_data = cursor.fetchall()
+        name_data = pd.DataFrame(name_data)
+        name_data.columns = [description[0] for description in cursor.description]
+        return name_data
+
+    def get_lang_row(self):
+        names_df = self.get_names_from_db()
+        eng_row = names_df[names_df['index'] == 'ENG']
+        ua_row = names_df[names_df['index'] == 'UKR']
+        ru_row = names_df[names_df['index'] == 'RU']
+
+        return eng_row, ua_row, ru_row
+
+    def create_lang_df(self, label_if_index=None, cols_name=None):
+        eng, ua, ru = self.get_lang_row()
+        eng.reset_index(drop=True, inplace=True)
+        ua.reset_index(drop=True, inplace=True)
+        ru.reset_index(drop=True, inplace=True)
+        data_ua = self.get_data_from_db_for_plot()
+        data_eng = self.get_data_from_db_for_plot()
+        data_ru = self.get_data_from_db_for_plot()
+        if label_if_index is not None:
+            eng_names = dict(zip(label_if_index, eng.loc[0, cols_name]))
+            ua_names = dict(zip(label_if_index, ua.loc[0, cols_name]))
+            ru_names = dict(zip(label_if_index, ru.loc[0, cols_name]))
+        else:
+            eng_names = dict(zip(self.labels, eng.loc[0, self.labels]))
+            ua_names = dict(zip(self.labels, ua.loc[0, self.labels]))
+            ru_names = dict(zip(self.labels, ru.loc[0, self.labels]))
+        data_ua.rename(columns=ua_names, inplace=True)
+        data_eng.rename(columns=eng_names, inplace=True)
+        data_ru.rename(columns=ru_names, inplace=True)
+        return data_ua, ua, data_eng, eng, data_ru, ru
+
+
---
+++ process_data/database.py
@@ -0,0 +1,89 @@
+import pandas as pd
+import psycopg2
+import Queries
+import const
+
+
+class ProcessToDB:
+    def __init__(self, labels, vals_qty, value_list, table, table_names, min_new_date=None,
+                 min_new_year=None, min_new_quoter=None, min_new_month=None, is_new=False, meta_labels=False):
+        self.is_new = is_new
+        self.labels = labels
+        self.vals_qty = vals_qty
+        self.value_list = value_list
+        self.table = table
+        self.table_names = table_names
+        self.min_new_date = min_new_date
+        self.min_new_month = min_new_month
+        self.min_new_year = min_new_year
+        self.min_new_quoter = min_new_quoter
+        self.meta_labels = meta_labels
+
+    @staticmethod
+    def connect_to_db():
+        host = "tradebot-db.cxsxzbu8uttc.eu-north-1.rds.amazonaws.com"
+        database = "postgres"
+        user = "postgres"
+        password = "gIoJwrSmBzx5qME4rh0I"
+        conn_tradebot = psycopg2.connect(
+            host=host,
+            database=database,
+            user=user,
+            password=password)
+        cursor_tradebot = conn_tradebot.cursor()
+        return conn_tradebot, cursor_tradebot
+
+    def create_table(self, query):
+        conn, cursor = self.connect_to_db()
+        cursor.execute(query)
+        conn.commit()
+        conn.close()
+
+    def add_data_to_db(self, table, labels=None, vals_qty=None, value_list=None):
+        conn, cursor = self.connect_to_db()
+        if labels is not None and vals_qty is not None and value_list is not None:
+            cursor.executemany(Queries.ADD_DATA_TO_DB.format(label_list=labels, val_qty=vals_qty, table=table), value_list)
+        else:
+            cursor.executemany(Queries.ADD_DATA_TO_DB.format(label_list=self.labels, val_qty=self.vals_qty, table=table), self.value_list)
+        conn.commit()
+        conn.close()
+
+    def update_db_rows_year_month(self):
+        my_query = None
+        if self.min_new_year is not None and self.min_new_month is not None and self.min_new_quoter is None and self.min_new_date is None:
+            my_query = Queries.UPDATE_DATA_BY_YEAR_MONTH.format(year=str(self.min_new_year),
+                                                                month=str(self.min_new_month),
+                                                                table=self.table)
+        elif self.min_new_year is not None and self.min_new_month is None and self.min_new_quoter is not None and self.min_new_date is None:
+            my_query = Queries.UPDATE_DATA_BY_YEAR_QUOTER.format(year=str(self.min_new_year),
+                                                                 quoter=str(self.min_new_quoter),
+                                                                 table=self.table)
+        elif self.min_new_year is not None and self.min_new_month is not None and self.min_new_quoter is not None and self.min_new_date is None:
+            my_query = Queries.UPDATE_DATA_BY_YEAR_QUOTER_MONTH.format(year=str(self.min_new_year),
+                                                                       quoter=str(self.min_new_quoter),
+                                                                       month=str(self.min_new_month),
+                                                                       table=self.table)
+        elif self.min_new_year is not None and self.min_new_month is not None and self.min_new_quoter is None and self.min_new_date is not None:
+            my_query = Queries.UPDATE_DATA_BY_YEAR_MONTH_DATE.format(year=str(self.min_new_year),
+                                                                     month=str(self.min_new_month),
+                                                                     date=str(self.min_new_date),
+                                                                     table=self.table)
+        elif self.min_new_year is not None and self.min_new_month is None and self.min_new_quoter is None and self.min_new_date is None:
+            my_query = Queries.UPDATE_DATA_BY_YEAR.format(year=str(self.min_new_year),
+                                                                     table=self.table)
+        conn, cursor = self.connect_to_db()
+        cursor.execute(my_query)
+        conn.commit()
+        conn.close()
+
+    def process_data_to_db(self, table, columns):
+        if self.is_new:
+            self.create_table(Queries.CREATE_QUERY.format(table=table,
+                                                          columns_with_types=columns))
+            self.add_data_to_db(table)
+        elif not self.is_new:
+            self.update_db_rows_year_month()
+            self.add_data_to_db(table)
+
+
+
